590551c5c1da5ea01d02f7db369231bc8bc62d07
seong-dodo <dyrnxm52@hanmail.net>
2021-10-04 21:17:02 +0900

    step1 - 검색폼 이벤트
    - 검색어를 입력하고 찾기 버튼을 클릭할 수 있다.
    - 입력값이 없는 상태에서 찾기 버튼 클릭라면 알림창을 띄워 사용자에게 알려준다.
    - 클릭을 하고 나면 입력폼 입력값이 초기화된다.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 /* eslint-disable no-alert */
 const $ = (selector) => document.querySelector(selector);
+
 function App() {
   $('#search-user-form').addEventListener('submit', (e) => {
     e.preventDefault();
   });
-  $('#search-user-form').addEventListener('keypress', (e) => {
+
+  $('#search-user').addEventListener('keypress', (e) => {
     if (e.key !== 'Enter') {
       return;
     }
     if (e.target.value === '') {
       alert('값을 입력해주세요.');
+      return;
     }
     // TODO : 추후 단계에서 입력값(e.target.value)찾기 구현 예정
     e.target.value = '';
+  });
+
+  $('#search-user-submit-button').addEventListener('click', () => {
+    if ($('#search-user').value === '') {
+      alert('값을 입력해주세요.');
+      return;
+    }
+    // TODO : 추후 단계에서 입력값 찾기 구현 예정
+
+    $('#search-user').value = '';
   });
 }
 
