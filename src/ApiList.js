b0f47afa0e62ee5321d96865b96c9bd9dd473ae0
seong-dodo <dyrnxm52@hanmail.net>
2021-10-05 22:42:09 +0900

    step2 - 즐겨찾기 해제
    - 즐겨찾기 된 사용자의 즐겨찾기 버튼을 클릭하면 로컬 저장소 저장 리스트에서 제외한다.
    - class="marked"가 지워지고 즐겨찾기 버튼의 배경색이 투명으로 변경된다.

--- src/ApiList.js
+++ src/ApiList.js
@@ -16,31 +16,63 @@ class ApiList {
   }
 
   createUser(users) {
-    const template = users.map((user) => `
-      <li class="api-item">
+    const template = users.map((user) => {
+      if (store.getLocalStorage() === null) {
+        return ` 
+        <li data-key-id=${user.id} class="api-item">
         <img class='user-img' src=${user.avatar_url}/>
         <div class='user-name'>${user.login}</div>
         <button class='bookmark-btn' type='button'>
           <i class="far fa-star fa-2x bookmark-btn"></i>
         </button>
-      </li>
-      `).join('');
+        </li>`;
+      }
+      if (store.getLocalStorage() !== null) {
+        const isLocal = store.getLocalStorage().find((target) => target.login === user.login);
+        if (isLocal || store.getLocalStorage() === null) {
+          return ` 
+          <li data-key-id=${user.id} class="api-item">
+          <img class='user-img' src=${user.avatar_url}/>
+          <div class='user-name'>${user.login}</div>
+          <button class='bookmark-btn' type='button'>
+            <i class="fas fa-star fa-2x bookmark-btn"></i>
+          </button>
+          </li>`;
+        }
 
+        if (!isLocal) {
+          return ` 
+          <li data-key-id=${user.id} class="api-item">
+          <img class='user-img' src=${user.avatar_url}/>
+          <div class='user-name'>${user.login}</div>
+          <button class='bookmark-btn' type='button'>
+            <i class="far fa-star fa-2x bookmark-btn"></i>
+          </button>
+          </li>`;
+        }
+      }
+    }).join('');
     $('#search-api-list').innerHTML = template;
   }
 
   addMark(e) {
     const bookmark = e.target.closest('li').querySelector('.fa-star');
-    bookmark.classList.add('fas');
     bookmark.classList.remove('far');
+    bookmark.classList.add('fas');
+  }
+
+  removeMark(e) {
+    const bookmark = e.target.closest('li').querySelector('.fa-star');
+    bookmark.classList.remove('fas');
+    bookmark.classList.add('far');
   }
 
   addLocalList(e) {
-    const imageUrl = e.target.closest('li').querySelector('.user-img').src;
+    const userImageUrl = e.target.closest('li').querySelector('.user-img').src;
     const userId = e.target.closest('li').querySelector('.user-name').innerText;
 
     const markedUser = {
-      avatar_url: imageUrl,
+      avatar_url: userImageUrl,
       login: userId,
     };
 
@@ -56,9 +88,33 @@ class ApiList {
     this.addMark(e);
   }
 
+  removeLocalList(e) {
+    if (store.getLocalStorage() !== null) {
+      const userId = e.target.closest('li').querySelector('.user-name').innerText;
+      const check = store.getLocalStorage().find((item) => item.login === userId);
+
+      if (check) {
+        const localList = store.getLocalStorage().filter((item) => item.login !== userId);
+        store.setLocalStorage(localList);
+        this.removeMark(e);
+      }
+    }
+  }
+
   initEvent() {
     $('#search-api-list').addEventListener('click', (e) => {
       if (e.target.classList.contains('bookmark-btn')) {
+        if (store.getLocalStorage() !== null) {
+          const userId = e.target.closest('li').querySelector('.user-name').innerText;
+          const check = store.getLocalStorage().find((item) => item.login === userId);
+
+          if (check) {
+            const localList = store.getLocalStorage().filter((item) => item.login !== userId);
+            store.setLocalStorage(localList);
+            this.removeMark(e);
+            return;
+          }
+        }
         this.addLocalList(e);
       }
     });
